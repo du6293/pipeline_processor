07e70d5cce9f85136130234587a79312ffa98a9a
seon deok Kim <76850241+du6293@users.noreply.github.com>
2023-04-19 02:22:13 +0900

    Update regfile.sv

--- regfile.sv
+++ regfile.sv
@@ -3,13 +3,13 @@
 module regfile
 #(  parameter   REG_WIDTH = 32 )    // the width of register file
 (
-    input           clk,
-    input   [4:0]   rs1,    // source register 1
-    input   [4:0]   rs2,    // source register 2
-    input   [4:0]   rd,     // destination register
-    input   [REG_WIDTH-1:0] rd_din,     // input data for rd
-    input           reg_write,      // RegWrite signal
-    output  [REG_WIDTH-1:0] rs1_dout,
+    input           clk                 ,
+    input   [4:0]   rs1                 ,    // source register 1
+    input   [4:0]   rs2                 ,    // source register 2
+    input   [4:0]   rd                  ,    // destination register
+    input   [REG_WIDTH-1:0] rd_din      ,    // input data for rd
+    input           reg_write           ,    // RegWrite signal
+    output  [REG_WIDTH-1:0] rs1_dout    ,
     output  [REG_WIDTH-1:0] rs2_dout
 );
 
@@ -17,8 +17,6 @@ module regfile
     logic   [REG_WIDTH-1:0] rf_data[0:31];
 
     // Write operation:
-    // - Note that clock is not used in the textbook, however in RTL design it can generate a infinite loop without clock.
-    // Thus, the write operation is done at the rising edge of clock
     always_ff @ (posedge clk) begin
         if (reg_write)
             rf_data[rd] <= rd_din;
@@ -27,10 +25,6 @@ module regfile
     // Read operation supporting internal forwarding
     assign rs1_dout = (reg_write & (rs1==rd)) ? rd_din: ((|rs1) ? rf_data[rs1]: 'b0);
     assign rs2_dout = (reg_write & (rs2==rd)) ? rd_din: ((|rs2) ? rf_data[rs2]: 'b0);
-
-    // Read operation (no internal forwarding)
-    //assign rs1_dout = (|rs1) ? rf_data[rs1]: 'b0;
-    //assign rs2_dout = (|rs2) ? rf_data[rs2]: 'b0;
 
 // synthesis translate_off
     initial begin
